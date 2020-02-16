50fc481d4bdb9144f5c44b9fdfa5ddb9ee8db4df
Paul Onyango <jorhnepaulone@gmail.com>
2020-02-16 20:17:21 +0300

    called my function on js

--- css/js/script.js
+++ css/js/script.js
@@ -1,3 +1,17 @@
-function myFunction(){
-    
+function myFunction(){  //calling
+    var century=document.getElementById("century");
+    var cc=parseInt("century")
+    var year=document.getElementById("year").value;
+    var yy=parseInt("year");
+    var month=document.getElementById("month").value;
+    var mm=parseInt("month");
+    var day=document.getElementById("day").value;
+    var dd=parseInt("day");
+    var male=document.getElementById("male");
+    var female=document.getElementById("female");
+
+   
+
+
+
 }
